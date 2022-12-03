4238b4a3bf8ac4595f6b0ee4d38462943f2a5120
Xander Deng <dengxiang2010@gmail.com>
2022-12-03 21:34:08 +0800

    poke

--- Sources/SplatNet3Helper/SN3WebViewData.swift
+++ Sources/SplatNet3Helper/SN3WebViewData.swift
@@ -1,129 +1,15 @@
 import Foundation
 import InkMoya
-import SwiftSoup
 
 #if canImport(FoundationNetworking)
     import FoundationNetworking
 #endif
 
-public typealias GraphQLHashMap = [String: String]
-
-public struct SN3WebViewData: Codable {
+public struct SN3WebViewData: Codable, Equatable {
     public let version: String
     public let graphql: GraphQL
 
-    public struct GraphQL: Codable {
-        public let hashMap: GraphQLHashMap
-
-        public init(hashMap: GraphQLHashMap) {
-            self.hashMap = hashMap
-        }
-    }
-
-    public init(version: String, graphql: GraphQL) {
-        self.version = version
-        self.graphql = graphql
-    }
-}
-
-extension SN3WebViewData: Equatable {
-    public static func == (lhs: SN3WebViewData, rhs: SN3WebViewData) -> Bool {
-        lhs.version == rhs.version &&
-        lhs.graphql == rhs.graphql
-    }
-}
-
-extension SN3WebViewData.GraphQL: Equatable { }
-
-public extension SN3Helper {
-    static func getWebViewData() async throws -> SN3WebViewData {
-        let webviewURL = URL(string: "https://api.lp1.av5ja.srv.nintendo.net")!
-
-        var req = URLRequest(url: webviewURL)
-        var (data, res) = try await session.request(req)
-        if res.httpURLResponse.statusCode != 200 {
-            throw Error.requestHtmlError(url: res.url)
-        }
-
-        guard let webHtml = String(data: data, encoding: .utf8) else {
-            throw Error.requestHtmlError(url: res.url)
-        }
-
-        let doc = try SwiftSoup.parse(webHtml)
-        let s = try doc.select("script[src*='static']")
-
-        guard let mainJsPath = try? s.first()?.attr("src") else {
-            throw Error.parseDataError(url:res.url)
-        }
-
-        req = URLRequest(url: webviewURL.appendingPathComponent(mainJsPath))
-        (data, res) = try await session.request(req)
-        if res.httpURLResponse.statusCode != 200 {
-            throw Error.requestHtmlError(url: res.url)
-        }
-
-        guard let jsContent = String(data: data, encoding: .utf8) else {
-            throw Error.parseDataError(url: res.url)
-        }
-
-        let sn3Data = SN3WebViewData(
-            version: try parseWebViewVersion(jsContent: jsContent),
-            graphql: SN3WebViewData.GraphQL(hashMap: try parseGraphQLHashMap(jsContent: jsContent))
-        )
-
-        return sn3Data
-    }
-
-    private static func parseWebViewVersion(jsContent: String) throws -> String {
-        guard
-            let match = try? NSRegularExpression(
-                pattern: #"=.(?<revision>[0-9a-f]{40}).*revision_info_not_set.*=.(?<version>\d+\.\d+\.\d+)-"#,
-                options: [])
-                .firstMatch(in: jsContent, range: NSRange(location: 0, length: jsContent.count))
-        else {
-            throw Error.parseDataError()
-        }
-
-        let revisionRange = match.range(withName: "revision")
-        let versionRange = match.range(withName: "version")
-
-        if revisionRange.location == NSNotFound ||
-            versionRange.location == NSNotFound {
-            throw Error.parseDataError()
-        }
-
-        let revision = NSString(string: jsContent).substring(with: revisionRange)
-        let version = NSString(string: jsContent).substring(with: versionRange)
-
-        return "\(version)-\(revision[revision.startIndex..<revision.index(revision.startIndex, offsetBy: 8)])"
-    }
-
-    private static func parseGraphQLHashMap(jsContent: String) throws -> [String: String] {
-        guard
-            let matchs = try? NSRegularExpression(
-                pattern: #"params:\{id:.(?<id>[0-9a-f]{32}).,metadata:\{\},name:.(?<name>[a-zA-Z0-9_]+).,"#,
-                options: [])
-                .matches(in: jsContent, range: NSRange(location: 0, length: jsContent.count))
-        else {
-            throw Error.parseDataError()
-        }
-
-        var hashMap = [String: String]()
-        for match in matchs {
-            let hashRange = match.range(withName: "id")
-            let nameRange = match.range(withName: "name")
-
-            if hashRange.location == NSNotFound ||
-                nameRange.location == NSNotFound {
-                throw Error.parseDataError()
-            }
-
-            let hash = NSString(string: jsContent).substring(with: hashRange)
-            let name = NSString(string: jsContent).substring(with: nameRange)
-
-            hashMap[name] = hash
-        }
-
-        return hashMap
+    public struct GraphQL: Codable, Equatable {
+        public let hashMap: [String: String]
     }
 }
